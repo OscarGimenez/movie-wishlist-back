3acca2b9bb01d1bed4fe822f9c059f36cb544d7c
Giménez Aldabas <o.gimenez.aldabas@accenture.com>
2019-01-10 13:13:02 +0100

    List All Movies API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/movie-wishlist/movies/1.0/list/all', async function (req, res, next) {
+  try {
+    let result = await axios.get('https://movie-wishlist-60107.firebaseio.com/movies.json');
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    res.json({
+      errorMsg: 'There was an error retrieving the movies'
+    });
+  }
 });
 
 module.exports = router;
