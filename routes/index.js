6144edc6f7625d949d66bed7da0d7bc11100b94a
Giménez Aldabas <o.gimenez.aldabas@accenture.com>
2019-01-15 13:21:42 +0100

    Wishlist API

--- routes/index.js
+++ routes/index.js
@@ -45,13 +45,32 @@ router.post('/user-movies/1.0/add', async function (req, res, next) {
     res.json({
       err: err
     });
-
   }
-
 });
 
 router.post('/user-movies/1.0/remove', async function (req, res, next) {
   try {
+    //FIXME: Try to handle better this
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.json(result.data);
+  } catch (error) {
+    console.error(error);
+    res.json({
+      errorMsg: 'There was an error retrieving the movies'
+    });
+  }
+});
+
+router.get('/user-movies/1.0/list/:user', async function (req, res, next) {
+  try {
+    //FIXME: Try to handle better this
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    let user = req.params.user;
+    let result = await axios.get(`https://movie-wishlist-60107.firebaseio.com/wishlist.json?orderBy="username"&equalTo="${user}"`);
     res.json(result.data);
   } catch (error) {
     console.error(error);
